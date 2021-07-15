78ca2c6c47d7b0904d53c2812c55391a27c75c18
Evgeniy <evgeniydayneko@gmail.com>
2021-07-16 01:29:33 +0400

    HW-2

--- script-6.js
+++ script-6.js
@@ -1,7 +1,7 @@
 "use strict"
 
-function factorial(n) {
-    return (n != 1) ? n * factorial(n - 1) : 1;
+function factorial(m) {
+    return (m != 1) ? m * factorial(m - 1) : 1;
   }
   
   console.log(factorial(10));
